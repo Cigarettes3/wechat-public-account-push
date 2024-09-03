59b1dcd4bd168f51544bbb39a840988577837a64
Cigarettes3 <107940954+Cigarettes3@users.noreply.github.com>
2024-09-03 15:42:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'cjc',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oCtb66HjFvtE1ODDYio6rASNty7w'
+      id: 'oCtb66HjFvtE1ODDYio6rASNty7w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'IODKEVivRxY0zSAV3Xup8__o2fFcHc7FGLBt8avX5ek',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
